d6e7315c1d573b51a98e87435d45c6912aab2875
MacBook Pro <xuchu.liu@northwestern.edu>
2020-08-24 11:33:05 -0500

    ignore pycache

--- Docker/batchQA.py
+++ Docker/batchQA.py
@@ -1,28 +1,12 @@
 # import flywheel package
 import flywheel
+from utils.fly.load_manifest_json import *
 
-context = flywheel.GearContext()  # Get the gear context.
-config = context.config           # from the gear context, get the config settings.
+def initialize(context):
 
-## Load in values from the gear configuration.
-my_name = config['my_name']
-num_rep = config['num_rep']
+    # Add manifest.json as the manifest_json attribute
+    setattr(context, 'manifest_json', load_manifest_json())
 
-# Create client
-fw = flywheel.Client('rush.trial.flywheel.io:FPzbtsrNQFRjDCNM8N')
-
-self = fw.get_current_user()
-print('I am %s %s' % (self.firstname, self.lastname))
-
-all_projects = fw.projects()
-
-project = fw.projects.find_first('label=QA')
-
-#session = project.sessions.find_first('label=Control1')
-# for subject in project.subjects.iter():
-#     print('%s: %s' % (subject.id, subject.label))
-
-for session in project.sessions():
-    print('%s: %s' % (session.id, session.label))
-    for acquisition in session.acquisitions():
-        print('    %s: %s' % (acquisition.id, acquisition.label))
+if __name__ == '__main__':
+    context = flywheel.GearContext()  # Get the gear context.
+    log = initialize(context)
